e4347c8476e950cdac7dcfaf296618e553bf775a
pixendrit <pixendrit@outlook.com>
2021-04-25 18:07:32 +0200

    clean branch

--- classwork/JavaScript_basics/04_comments.js
+++
@@ -1,9 +0,0 @@
-let age = 22; // Declare age, git it the value of 22
-// console.log(age);
-
-/*
-The code below will declare a variable
-and after assign a value to it
-*/
-let message;
-message = 'Hello, World!';
